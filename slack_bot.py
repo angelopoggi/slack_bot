b96a591c1299e8db0b0b8407801de98c1011865c
Angelo Poggi <angelo.poggi@webair.com>
2021-12-21 13:28:53 -0500

    scheduler

--- slack_bot.py
+++ slack_bot.py
@@ -11,18 +11,21 @@ def bill_bot():
     currentTime = datetime.datetime.now()
     client = slack.WebClient(token=SLACK_TOKEN)
     if currentTime.hour == 9:
-        client.chat_postMessage(channel='#provisioninginternal', text="Morning Everyone!")
+        client.chat_postMessage(channel='#bot-testing', text="Morning Everyone!")
     if currentTime.hour == 16:
-        client.chat_postMessage(channel='#provisioninginternal', text="Night Everyone!")
+        client.chat_postMessage(channel='#bot-testing', text="Night Everyone!")
     randomTime = random.randrange(9,15,1)
     if currentTime.hour == randomTime:
         currentJoke = dad_jokes.dad_jokes()
-        client.chat_postMessage(channel='#provisioninginternal', text=currentJoke[0])
+        client.chat_postMessage(channel='#bot-testing', text=currentJoke[0])
         time.sleep(2)
-        client.chat_postMessage(channel='#provisioninginternal', text=currentJoke[1])
+        client.chat_postMessage(channel='#bot-testing', text=currentJoke[1])
 
 if __name__== '__main__':
-    schedule.every().day.at("09:00:").do(bill_bot())
+    #Morning run
+    schedule.every().day.at("09:00:00").do(bill_bot)
+    #Evening run
+    schedule.every().day.at("17:00:00").do(bill_bot)
     while True:
         schedule.run_pending()
         time.sleep(60)
