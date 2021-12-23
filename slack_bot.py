a08476d9a82f048f0483205294583bb07b9d1732
Angelo Poggi <angelo.poggi@webair.com>
2021-12-23 09:31:45 -0500

    removed joke thing for now

--- slack_bot.py
+++ slack_bot.py
@@ -11,14 +11,9 @@ def bill_bot(random=False):
     currentTime = datetime.datetime.now()
     client = slack.WebClient(token=SLACK_TOKEN)
     if currentTime.hour == 9:
-        client.chat_postMessage(channel='#bot-testing', text="Morning Everyone!")
+        client.chat_postMessage(channel='#provisioninginternal', text="Morning Everyone!")
     if currentTime.hour == 16:
-        client.chat_postMessage(channel='#bot-testing', text="Night Everyone!")
-    if random:
-        currentJoke = dad_jokes.dad_jokes()
-        client.chat_postMessage(channel='#bot-testing', text=currentJoke[0])
-        time.sleep(2)
-        client.chat_postMessage(channel='#bot-testing', text=currentJoke[1])
+        client.chat_postMessage(channel='#provisioninginternal', text="Night Everyone!")
         
 def random_num_gen():
     randomTime = random.randrange(9, 15, 1)
@@ -26,12 +21,6 @@ def random_num_gen():
 if __name__== '__main__':
     #Morning run
     schedule.every().day.at("09:00:00").do(bill_bot)
-    randomTime = random_num_gen()
-    print(randomTime)
-    if randomTime == 9:
-        schedule.every().day.at(f"0{randomTime}:00:00").do(bill_bot,random=True)
-    elif randomTime in range(10,15):
-        schedule.every().day.at(f"{randomTime}:00:00").do(bill_bot,random=True)
     #Evening run
     schedule.every().day.at("17:00:00").do(bill_bot)
     while True:
